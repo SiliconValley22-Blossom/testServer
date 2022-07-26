7c1c7c319c193c522b5f3f65b9034682747169bd
Hyeonjun Park <phjppo0918@gmail.com>
2022-07-26 16:13:00 +0900

    Auto stash before merge of "main" and "origin/main"

--- app.py
+++ app.py
@@ -6,15 +6,13 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 
-@app.route('/api/login', methods=['post'])
-def postLogin():  # put application's code here
-    print(request.get_json())
-    
+@app.route('/api/login')
+def hello_world():  # put application's code here
     return """
     {
     "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY1ODI5MTUxOCwianRpIjoiZGY3MWYwMjctMmE5Yy00ZWEwLTkyMDUtYTA0ZWU5YTA1NTFkIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6InFAcXEucSIsIm5iZiI6MTY1ODI5MTUxOCwiZXhwIjoxNjU4MjkyNzE4fQ.K0F__RL0flRzZmvQba5WDXzko8Lrt52OH0uHlUMGEPA",
     "refresh_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY1ODI5MTUxOCwianRpIjoiNGNjNWM3ZjYtZWUzOS00NzRjLWFhM2UtZGY1NTEzMDg0NTNhIiwidHlwZSI6InJlZnJlc2giLCJzdWIiOiJxQHFxLnEiLCJuYmYiOjE2NTgyOTE1MTgsImV4cCI6MTY1OTUwMTExOH0.Jr1epo4dLFzZsOAwFVWBUu2o9pJICWDxWU724IO32iM"
-    }
+}
     """
 
 @app.route('/api/users', methods=['post'])
@@ -23,29 +21,33 @@ def apiUsers():  # put application's code here
     
     return "ok"
 
+@app.route('/api/users', methods=['post'])
+def jkl():
+    print(request)
+    print(request.data)
+    print(request.data['email'])
+    print(request.data['password'])
+    print(request.data['nickname'])
+    return 'ok'
+
 
 @app.route('/api/photos', methods=['post'])
 def postPhotos():
     file = request.files['file']
     print(file.filename)
     print(file.content_type)
-    return """
-        {
-           " black_photo_id":1,
-           " color_photo_id":2
-        }
-
-    """
+    return 'created'
 
 
 @app.route('/api/photos/<int:photo_id>', methods=['get'])
-def getPhotosById():
+def getPhotosById(photo_id):
+
     result = {"photo": 'test/newFile.png'}
     return result
 
 
 @app.route('/api/photos', methods=['get'])
-def getPhotos():
+def posttPhotos():
     return """
     {
     "photo_list": [
