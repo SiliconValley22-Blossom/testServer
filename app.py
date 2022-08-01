66e2695bfb816912bcc068717b041467e9c2b5de
sm000117 <75351686+ParkSeonMi@users.noreply.github.com>
2022-08-01 14:38:12 +0900

    add api

--- app.py
+++ app.py
@@ -42,31 +42,29 @@ def postPhotos():
     file = request.files['file']
     print(file.filename)
     print(file.content_type)
-    return """
-    {
-    "black_photo_id":1,
-    "color_photo_id":2  
-}
-    """
+    return jsonify({
+        "black_photo_id":1,
+        "color_photo_id":2  
+    })
+
 @app.route('/api/users', methods=['get'])
 def getUser():
 
-    return """
-    {
+    return jsonify({
          "user_id": 0,
           "email": "string",
           "nickname": "string",
           "created_at": "2022-07-31T09:17:18.078Z",
           "updated_at": "2022-07-31T09:17:18.078Z",
-          "is_deleted": true
-    }
-    """
+          "is_deleted": True
+    })
+
 
 @app.route('/api/users', methods=['patch'])
 def patchUser():
     print(request.data['password'])
     print(request.data['new_password'])
-    return "ok"
+    return "200"
 
 
 
@@ -81,59 +79,49 @@ def getPhotosById(photo_id):
 def posttPhotos():
     param = request.args.get('userId')
     if param is None:
-        return """
-{
-    "photo_list": [
-        [
-            "black/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "color/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "black/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "color/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "black/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "color/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "black/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "color/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+        return jsonify({
+            "photo_list": [
+                {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            },
+            {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            },
+            {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            }
+            ]
+        })
+
+    return jsonify({
+        "photo_list": [
+            {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            },
+            {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            },
+            {
+                "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
+                "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
+            }
         ]
-    ]
-}
-    """
-    return """
-{
-    "photo_list": [
-        [
-            "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ],
-        [
-            "color/1360f998-3ecd-431f-a0e1-ce607bf7d320.jpeg",
-            "black/74f90258-6e8c-4f0f-8e03-908f1c79787a.jpeg"
-        ]
-    ]
-}
-    """
+    })
+
 
 @app.route('/api/admin/users', methods=['get'])
 def getAdmin():
-    return """
+    return jsonify(
     [
         {
             "created_at": "Thu, 21 Jul 2022 12:46:36 GMT",
             "email": "test@google.com",
-            "is_deleted": false,
+            "is_deleted": False,
             "nickname": "TEST",
             "updated_at": "Thu, 21 Jul 2022 12:46:36 GMT",
             "user_id": 7
@@ -141,7 +129,7 @@ def getAdmin():
         {
             "created_at": "Fri, 22 Jul 2022 06:44:45 GMT",
             "email": "aaa1",
-            "is_deleted": false,
+            "is_deleted": False,
             "nickname": "test",
             "updated_at": "Fri, 22 Jul 2022 06:44:45 GMT",
             "user_id": 8
@@ -149,24 +137,30 @@ def getAdmin():
         {
             "created_at": "Tue, 26 Jul 2022 06:04:05 GMT",
             "email": "asdf",
-            "is_deleted": false,
+            "is_deleted": False,
             "nickname": "123",
             "updated_at": "Tue, 26 Jul 2022 06:04:05 GMT",
             "user_id": 9
         }
     ]
-    """
+    )
+
 @app.route('/api/admin/users', methods=['delete'])
 def deleteUser():
-    return 204
+    return "204"
 
 @app.route('/api/login/check', methods=['get'])
 def checkLogin():
     return jsonify({"is_login":True})
 
-@app.route('/api/users/reset-pw', methods=['post'])
+@app.route('/api/users/reset-password', methods=['post'])
 def resetPw():
-    return 200
+    return "200"
+
+@app.route('/api/refresh', methods=['get'])
+def refreshToken():
+    return "200"
+
 
 if __name__ == '__main__':
     app.run()
